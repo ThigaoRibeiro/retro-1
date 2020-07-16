80c731ecc9cb7dffc13209558e6a375e92bb6de2
Yuri do Monte <yuri.spm@gmail.com>
2020-07-16 20:51:02 -0300

    Banco Atualizado

--- Sistema/BD/retro_db.sql
+++ Sistema/BD/retro_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 15/07/2020 às 22:12
+-- Tempo de geração: 16/07/2020 às 20:49
 -- Versão do servidor: 10.1.44-MariaDB-0ubuntu0.18.04.1
 -- Versão do PHP: 7.4.7
 
@@ -57,7 +57,20 @@ CREATE TABLE `pedido` (
   `data_vend` date NOT NULL,
   `valor_vend` float NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `id_funcionario` int(11) NOT NULL
+  `id_funcionario` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id_produto` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `valor_prod` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -83,7 +96,14 @@ ALTER TABLE `pedido`
   ADD PRIMARY KEY (`id_pedido`),
   ADD KEY `id_cliente` (`id_cliente`),
   ADD KEY `id_cliente_2` (`id_cliente`),
-  ADD KEY `id_funcionario` (`id_funcionario`);
+  ADD KEY `id_funcionario` (`id_funcionario`),
+  ADD KEY `id_produto` (`id_produto`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id_produto`);
 
 --
 -- AUTO_INCREMENT de tabelas apagadas
@@ -108,6 +128,12 @@ ALTER TABLE `pedido`
   MODIFY `id_pedido` int(100) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restrições para dumps de tabelas
 --
 
@@ -115,6 +141,7 @@ ALTER TABLE `pedido`
 -- Restrições para tabelas `pedido`
 --
 ALTER TABLE `pedido`
+  ADD CONSTRAINT `id_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`),
   ADD CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`),
   ADD CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario` (`id_funcionario`);
 COMMIT;
