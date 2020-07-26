ef2835c89cd10ac6a2b1df0bec32e3169a1509be
Thiago Ribeiro <46843472+ThigaoRibeiro@users.noreply.github.com>
2020-07-25 21:42:33 -0300

    projeto bistro

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+  
+    require_once __DIR__ . "/Sistema/Sistemas/Database.php";
+    $p =new Databases("retro_db","localhost","yuri","7q5eb6eb");
+
+
+?>
+
+<!DOCTYPE html>
+<thml lang="pt-br">
+<head>
+    <meta charset="uft-8"/>
+    <title>Retro</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <h2>SISTEMAS DE PEDIDOS</h2>
+    <section id="esquerda">
+        <h5>RETRO</h5>
+        <h6>Desde 1992</h6>
+        
+
+    </section>
+    <section id="direita">
+        <div id="rectangle"></div>
+
+
+    </section>
+ 
+</select>
+</body>
+
+</thml>
