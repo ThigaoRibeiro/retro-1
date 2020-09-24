99cbf7f36ef8ac293c227b089ac630b441ab590f
Yuri do Monte <yuri.spm@gmail.com>
2020-09-23 22:38:40 -0300

    Acerto2

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ require_once 'vendor/autoload.php';
 
 $model = new \Source\CRUD\Models\UserModel();
 
-<<<<<<< HEAD
 $user = $model->loadCliente(1);
 var_dump(
     $user
 );
-=======
-$user = $model->load(1);
->>>>>>> 34a492e643032ed77d6263f8243e319ae901a62c
