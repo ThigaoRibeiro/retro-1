5d22606f1d7d348fd11741514143225a7eb11580
Yuri do Monte <yuri.spm@gmail.com>
2020-10-28 16:13:55 -0300

    Delete OK

--- index.php
+++ index.php
@@ -7,16 +7,9 @@ use Source\CRUD\Models\UserModel;
 require_once 'vendor/autoload.php';
 
 $model = new UserModel();
-
-$$user = $model->loadCliente(4);
-$user ->email = "gabriel2.santos@gmail.com";
-if ($user != $model->loadCliente(4)) {
-    $user->save();
-    echo "<p class='trigger warning'>Atualizado!</p>";
-} else {
-    echo "<p class='trigger accept'>Já atualizado!</p>";
+$user = $model->loadCliente(9);
+if ($user) {
+    $user->destroy();
 }
 
-
 var_dump($user);
-
